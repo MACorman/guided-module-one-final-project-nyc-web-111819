0653ff967acd384faad40807c7a2f256d4165227
kevinkim1030 <kevin.kim1030@gmail.com>
2019-12-04 09:20:14 -0500

    updated seeds

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,24 +1,24 @@
-#require 'rest-client'
-require 'json'
-#require 'HTTParty'
-#require 'Nokogiri'
+# #require 'rest-client'
+# require 'json'
+# #require 'HTTParty'
+# #require 'Nokogiri'
 
 
 
-@url = 'https://www.nutritionix.com/food/superone-foods-'
+# @url = 'https://www.nutritionix.com/food/superone-foods-'
 
 
-def get_data_from_api(user_input)
-   response = RestClient.get('https://flatironschool.com/?utm_source=Google&utm_medium=ppc&utm_campaign=1783325438&utm_content=75897432663&utm_term=flatiron%20school&uqaid=393386639725&CjwKCAiArJjvBRACEiwA-WiqqysUEWgTHlwwy7v7cjolAV_3JbyRfmReEHHdaY_eY147Tqo51VOGRBoC8GQQAvD_BwE&gclid=CjwKCAiArJjvBRACEiwA-WiqqysUEWgTHlwwy7v7cjolAV_3JbyRfmReEHHdaY_eY147Tqo51VOGRBoC8GQQAvD_BwE')
-    # res_hash = JSON.parse(response)
-    # binding.pry
+# def get_data_from_api(user_input)
+#    response = RestClient.get('https://flatironschool.com/?utm_source=Google&utm_medium=ppc&utm_campaign=1783325438&utm_content=75897432663&utm_term=flatiron%20school&uqaid=393386639725&CjwKCAiArJjvBRACEiwA-WiqqysUEWgTHlwwy7v7cjolAV_3JbyRfmReEHHdaY_eY147Tqo51VOGRBoC8GQQAvD_BwE&gclid=CjwKCAiArJjvBRACEiwA-WiqqysUEWgTHlwwy7v7cjolAV_3JbyRfmReEHHdaY_eY147Tqo51VOGRBoC8GQQAvD_BwE')
+#     # res_hash = JSON.parse(response)
+#     # binding.pry
 
-    # response = HTTParty.get(@url + user_input)
-    @parse_page = Nokogiri::HTML(response)
-    # binding.pry
-     puts "DID THE STUFF"
-    #binding.pry
-end
+#     # response = HTTParty.get(@url + user_input)
+#     @parse_page = Nokogiri::HTML(response)
+#     # binding.pry
+#      puts "DID THE STUFF"
+#     #binding.pry
+# end
 
 
 
